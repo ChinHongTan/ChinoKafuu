12bc62bc9278704bfa9e7391d0a81c46e64c17d8
ChinHongTan <52919589+ChinHongTan@users.noreply.github.com>
2021-01-08 21:33:01 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,6 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
-	cooldown: 5,
 	execute(message, args) {
 		return message.channel.send(`Latency: **\`${this.client.ws.ping}ms\`**`);
 	},
