7f7820385c57fc6266ce0734bd02273b5db68a33
ChinHongTan <52919589+ChinHongTan@users.noreply.github.com>
2021-01-17 22:13:52 +0800

    Update ChinoKafuu.js

--- ChinoKafuu.js
+++ ChinoKafuu.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const ytdl = require("ytdl-core");
 const { prefix, token } = require('./config.json');
+const ytdl = require("ytdl-core");
+const ytsr = require('ytsr');
 
 let rawData = fs.readFileSync('./editSnipes.json');
 let editSnipes = JSON.parse(rawData);
 let data = fs.readFileSync('./snipes.json');
 let snipes = JSON.parse(data);
-
+const queue = new Map();
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -23,6 +24,14 @@ for (const file of commandFiles) {
 }
 
 const cooldowns = new Discord.Collection();
+
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
+});
+
+client.once("disconnect", () => {
+  console.log("Disconnect!");
+});
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -61,10 +70,12 @@ client.on('messageDelete', message => {
 			channel.send(embed);
 		});
 		snipes.push(snipe);
+		if (snipes.length > 10) snipes.shift();
         let data = JSON.stringify(snipes, null, 2);
         fs.writeFileSync(`./snipes.json`, data);
 	} else {
 		snipes.push(snipe);
+		if (snipes.length > 10) snipes.shift();
 		let data = JSON.stringify(snipes, null, 2);
         fs.writeFileSync(`./snipes.json`, data);
 	};
@@ -79,17 +90,19 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
 	editSnipe.author = newMessage.author.tag;
 	editSnipe.authorAvatar = newMessage.author.displayAvatarURL({ format: "png", dynamic: true});
 	editSnipe.content = oldMessage.content;
-	editSnipe.timestamp = newMessage.editedAt.toUTCString([8]);
+  if (newMessage.editedAt) editSnipe.timestamp = newMessage.editedAt.toUTCString([8]);
 	editSnipes.push(editSnipe);
+	if (editSnipes.length > 10) editSnipes.shift();
 	let data = JSON.stringify(editSnipes, null, 2);
 	fs.writeFileSync(`./editSnipes.json`, data);
 });
+
 
 client.on('message', message => {
 	if (message.author.bot || !message.content.startsWith(prefix)) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
+  const commandName = args.shift().toLowerCase();
 
 	const command = client.commands.get(commandName)
 		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
@@ -128,5 +141,123 @@ client.on('message', message => {
 	};
 	return;
 });
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const serverQueue = queue.get(message.guild.id);
+
+  if (message.content.startsWith(`${prefix}play`)) {
+    execute(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}skip`)) {
+    skip(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}stop`)) {
+    stop(message, serverQueue);
+    return;
+  }; 
+});
+
+async function execute(message, serverQueue) {
+  const args = message.content.split(" ");
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "You need to be in a voice channel to play music!"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "I need the permissions to join and speak in your voice channel!"
+    );
+  };
+
+  if (ytdl.validateURL(args[1])) {
+    var link = args[1];
+  } else {
+    var keyword = message.content.substr(message.content.indexOf(' ') + 1);
+    message.channel.send(`Searching ${keyword}...`)
+    const searchResults = await ytsr(keyword, {gl: 'TW', gl: 'zh', limit: 30});
+    var link = searchResults.items[0].url;
+  };
+  const songInfo = await ytdl.getInfo(link);
+  const song = {
+        title: songInfo.videoDetails.title,
+        url: songInfo.videoDetails.video_url,
+   };
+
+  if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+
+    queue.set(message.guild.id, queueContruct);
+
+    queueContruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
+    };
+  } else {
+    serverQueue.songs.push(song);
+    return message.channel.send(`${song.title} has been added to the queue!`);
+  };
+};
+
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+  if (!serverQueue)
+    return message.channel.send("There is no song that I could skip!");
+  serverQueue.connection.dispatcher.end();
+};
+
+function stop(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+    
+  if (!serverQueue)
+    return message.channel.send("There is no song that I could stop!");
+    
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+};
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  };
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url, {quality: 'highestaudio', highWaterMark : 1 << 25 } ))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("error", error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+};
 
 client.login(token);
