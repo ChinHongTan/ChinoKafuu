0d3ae7eb95e15cb61a9d5d661730dc2cfb66aa9d
ChinHongTan <52919589+ChinHongTan@users.noreply.github.com>
2021-01-22 23:01:25 +0800

    Delete user-info.js

--- user-info.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'user-info',
-	aliases: ['user'],
-	description: "User's information ",
-	execute(message, args) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}\nDate created: ${message.author.createdAt}\nYour Tag: ${message.author.tag}`);
-	},
-};
