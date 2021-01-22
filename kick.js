8335fa411ce7bb1a63756abe7577f185df53383c
ChinHongTan <52919589+ChinHongTan@users.noreply.github.com>
2021-01-22 22:53:50 +0800

    Add files via upload

---
+++ kick.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'kick',
+	description: 'Kick someone out.',
+	guildOnly: true,
+	execute(message, args) {
+		if (!message.mentions.users.size) {
+	        return message.reply(`you need to tag a user in order to kick them!`); 
+        }
+	    const taggedUser = message.mentions.users.first();
+
+	    message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+	},
+};
