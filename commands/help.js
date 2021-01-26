90b743f9c464f68759575608336e6d3ff01a0cf1
ChinHongTan <52919589+ChinHongTan@users.noreply.github.com>
2021-01-26 20:05:34 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
         const { commands } = message.client;
 
 		if (!args.length) {
-			message.channel.send('Here\'s my invite link: https://discord.com/api/oauth2/authorize?client_id=781328218753859635&permissions=0&scope=bot');
+			message.channel.send('Here\'s my invite link: https://discord.com/api/oauth2/authorize?client_id=781328218753859635&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D781328218753859635%26permissions%3D8%26redirect_uri%3Dhttps%253A%252F%252Fdiscord.com%252Fapi%252Foauth2%252Fauthorize%253Fclient_id%253D781328&scope=bot%20applications.commands');
 			data.push('Here\'s a list of all my commands:');
 			data.push(commands.map(command => command.name).join(', '));
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
