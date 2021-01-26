55acfe61139e36d48279bd615ad72b3d5a032d5d
ChinHongTan <52919589+ChinHongTan@users.noreply.github.com>
2021-01-26 20:34:59 +0800

    Add files via upload

--- ChinoKafuu.js
+++ ChinoKafuu.js
@@ -154,7 +154,35 @@ client.on("message", async message => {
       message.channel.send(embed);
       message.delete();
     };
+  } else if (message.channel.id === '803270261604352040') {
+    let rawData = fs.readFileSync('./countingData.json');
+    let countingData = JSON.parse(rawData);
+    if (!Number(message.content)) return;
+    if (countingData.counter == '0' || !countingData.counter) {
+      if (message.content != '1') return;
+      countingData.author = message.author.tag;
+      countingData.counter = message.content;
+      let data = JSON.stringify(countingData, null, 2);
+      fs.writeFileSync('./countingData.json', data);
+      return message.react('✅');
+    };
+    if (message.author.tag != countingData.author) {
+      if (message.content != Number(countingData.counter) + 1) {
+        countingData.counter = '0';
+        countingData.author = '';
+        let data = JSON.stringify(countingData, null, 2);
+        fs.writeFileSync('./countingData.json', data);
+        return message.react('❌');
+      } else {
+        countingData.counter = message.content;
+        countingData.author = message.author.tag;
+        let data = JSON.stringify(countingData, null, 2);
+        fs.writeFileSync('./countingData.json', data);
+        return message.react('✅');
+      };
+    };
   };
+
   if (!message.content.startsWith(prefix)) return;
 
   const serverQueue = queue.get(message.guild.id);
