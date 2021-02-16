fec407e7964a13e6d4d8bac6577b210499ec0323
ChinHongTan <chinhongtan1@gmail.com>
2021-02-16 11:21:29 +0800

    User Info system updated

--- commands/user-info.js
+++ commands/user-info.js
@@ -9,16 +9,16 @@ module.exports = {
 			const profileWithTimestamp = JSON.parse(rawData);
 			const timestamp = Object.keys(profileWithTimestamp)[0];
 			const memberProfiles = profileWithTimestamp[Object.keys(profileWithTimestamp)[0]];
+			message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}\nDate created: ${message.author.createdAt}\nYour Tag: ${message.author.tag}`);
+			for(const profile of memberProfiles) {
+				if (profile.name != message.author.username) continue;
+				message.channel.send(`User Info: ${JSON.stringify(profile, null, 2)}`);
+				break;
+			};
+			return message.channel.send(`Last updated: ${timestamp}`); 
 		} catch (error) {
 			console.error(error);
 			return message.channel.send("An error occured when reading the file!");
 		}
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}\nDate created: ${message.author.createdAt}\nYour Tag: ${message.author.tag}`);
-		for(const profile of memberProfiles) {
-			if (profile.name != message.author.username) continue;
-			message.channel.send(`User Info: ${JSON.stringify(profile, null, 2)}`);
-			break;
-		};
-		return message.channel.send(`Last updated: ${timestamp}`); 
 	},
 };
