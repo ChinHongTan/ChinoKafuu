1ee1fdd24177ddc0b19d5f489a74f47ca7162e50
ChinHongTan <52919589+ChinHongTan@users.noreply.github.com>
2021-03-08 12:14:16 +0800

    database

--- dbInit.js
+++ dbInit.js
@@ -1,11 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	storage: 'database.sqlite',
-});
+const sequelize = new Sequelize('postgres://qtravabjlpauov:2b929125d81a36a17c9d92aed00aad154ffb65eca7c0f3fe3f1b9d021451e430@ec2-54-198-252-9.compute-1.amazonaws.com:5432/d1j44igonolhjv');
 
 const CurrencyShop = require('./models/CurrencyShop')(sequelize, Sequelize.DataTypes);
 require('./models/Users')(sequelize, Sequelize.DataTypes);
