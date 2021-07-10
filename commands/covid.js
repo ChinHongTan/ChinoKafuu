462abea5a7c05962891895529836fee249fc0e93
Chin Hong Tan <chinhongtan1@gmail.com>
2021-07-10 17:38:41 +0800

    Update covid.js

--- commands/covid.js
+++ commands/covid.js
@@ -8,12 +8,12 @@ module.exports = {
             baseUrl: "https://disease.sh",
         });
 
-        function createEmbed(result, page) {
-            const date = new Date(result[page].updated);
+        function createEmbed(result) {
+            const date = new Date(result.updated);
             const embed = new Discord.MessageEmbed()
                 .setColor("RANDOM")
                 .setTitle(
-                    `**${result[page].country} COVID-19 data  #${page + 1}**`
+                    `**${result.country} COVID-19 data**`
                 )
                 .setAuthor(
                     "ChinoKafuu",
@@ -22,51 +22,51 @@ module.exports = {
                 .addFields(
                     {
                         name: "Total cases",
-                        value: result[page].cases.toLocaleString(),
+                        value: result.cases.toLocaleString(),
                         inline: true,
                     },
                     {
                         name: "Confirmed today",
-                        value: result[page].todayCases.toLocaleString(),
+                        value: result.todayCases.toLocaleString(),
                         inline: true,
                     },
                     {
                         name: "Total deaths",
-                        value: result[page].deaths.toLocaleString(),
+                        value: result.deaths.toLocaleString(),
                         inline: true,
                     },
                     {
                         name: "Deaths today",
-                        value: result[page].todayDeaths.toLocaleString(),
+                        value: result.todayDeaths.toLocaleString(),
                         inline: true,
                     },
                     {
                         name: "Total recovered",
-                        value: result[page].recovered.toLocaleString(),
+                        value: result.recovered.toLocaleString(),
                         inline: true,
                     },
                     {
                         name: "Recovered today",
-                        value: result[page].todayRecovered.toLocaleString(),
+                        value: result.todayRecovered.toLocaleString(),
                         inline: true,
                     },
                     {
                         name: "Active cases",
-                        value: result[page].active.toLocaleString(),
+                        value: result.active.toLocaleString(),
                         inline: true,
                     },
                     {
                         name: "Critical cases",
-                        value: result[page].critical.toLocaleString(),
+                        value: result.critical.toLocaleString(),
                         inline: true,
                     },
                     {
                         name: "Population",
-                        value: result[page].population.toLocaleString(),
+                        value: result.population.toLocaleString(),
                         inline: true,
                     }
                 )
-                .setThumbnail(result[page].countryInfo.flag)
+                .setThumbnail(result.countryInfo.flag)
                 .setFooter(
                     `Requested by: ${
                         message.author.tag
@@ -74,6 +74,44 @@ module.exports = {
                     message.author.avatarURL()
                 );
             return embed;
+        }
+
+        function updateEmbed(r, page, result) {
+            if (r.emoji.name === "⬅️") {
+                page -= 1;
+                if (page < 0) page = result.length - 1;
+                let editedEmbed = createEmbed(result[page]);
+                embedMessage.edit(editedEmbed);
+            } else if (r.emoji.name === "➡️") {
+                page += 1;
+                if (page + 1 > result.length) page = 0;
+                let editedEmbed = createEmbed(result[page]);
+                embedMessage.edit(editedEmbed);
+            }
+        }
+
+        function createEmbedFlip(result) {
+            let page = 0;
+            let embed = createEmbed(result[page]);
+            message.channel.send(embed).then((embedMessage) => {
+                embedMessage
+                    .react("⬅️")
+                    .then(embedMessage.react("➡️"));
+                const filter = (reaction, user) =>
+                    ["⬅️", "➡️"].includes(reaction.emoji.name) &&
+                    !user.bot;
+                const collector =
+                    embedMessage.createReactionCollector(filter, {
+                        idle: 60000,
+                        dispose: true,
+                    });
+                collector.on("collect", (r) => {
+                    updateEmbed(r, page, result);
+                });
+                collector.on("remove", (r) => {
+                    updateEmbed(r, page, result);
+                });
+            });
         }
 
         if (args.length < 1) {
@@ -86,68 +124,8 @@ module.exports = {
         if (args[0] === "all") {
             // get global covid data
             api.all({ allowNull: false }).then((result) => {
-                console.log(result);
-                const date = new Date(result.updated);
-                const embed = new Discord.MessageEmbed()
-                    .setColor("RANDOM")
-                    .setTitle("**Global COVID-19 data**")
-                    .setAuthor(
-                        "ChinoKafuu",
-                        "https://cdn.discordapp.com/avatars/781328218753859635/af716f0a9958679bdb17edfc0add53a6.png?size=256"
-                    )
-                    .addFields(
-                        {
-                            name: "Total cases",
-                            value: result.cases.toLocaleString(),
-                            inline: true,
-                        },
-                        {
-                            name: "Confirmed today",
-                            value: result.todayCases.toLocaleString(),
-                            inline: true,
-                        },
-                        {
-                            name: "Total deaths",
-                            value: result.deaths.toLocaleString(),
-                            inline: true,
-                        },
-                        {
-                            name: "Deaths today",
-                            value: result.todayDeaths.toLocaleString(),
-                            inline: true,
-                        },
-                        {
-                            name: "Total recovered",
-                            value: result.recovered.toLocaleString(),
-                            inline: true,
-                        },
-                        {
-                            name: "Recovered today",
-                            value: result.todayRecovered.toLocaleString(),
-                            inline: true,
-                        },
-                        {
-                            name: "Active cases",
-                            value: result.active.toLocaleString(),
-                            inline: true,
-                        },
-                        {
-                            name: "Critical cases",
-                            value: result.critical.toLocaleString(),
-                            inline: true,
-                        },
-                        {
-                            name: "Population",
-                            value: result.population.toLocaleString(),
-                            inline: true,
-                        }
-                    )
-                    .setFooter(
-                        `Requested by: ${
-                            message.author.tag
-                        }\nData updated: ${date.toUTCString()}`,
-                        message.author.avatarURL()
-                    );
+                result.country = "Global";
+                let embed = createEmbed(result);
                 return message.channel.send(embed);
             });
         }
@@ -157,48 +135,7 @@ module.exports = {
                 // get a list of data of all countries sorted by cases
                 api.countries({ sort: "cases", allowNull: false }).then(
                     (result) => {
-                        console.log(result);
-                        var page = 0;
-                        let embed = createEmbed(result, page);
-                        message.channel.send(embed).then((embedMessage) => {
-                            embedMessage
-                                .react("⬅️")
-                                .then(embedMessage.react("➡️"));
-                            const filter = (reaction, user) =>
-                                ["⬅️", "➡️"].includes(reaction.emoji.name) &&
-                                !user.bot;
-                            const collector =
-                                embedMessage.createReactionCollector(filter, {
-                                    idle: 60000,
-                                    dispose: true,
-                                });
-                            collector.on("collect", (r) => {
-                                if (r.emoji.name === "⬅️") {
-                                    page -= 1;
-                                    if (page < 0) page = result.length - 1;
-                                    var editedEmbed = createEmbed(result, page);
-                                    embedMessage.edit(editedEmbed);
-                                } else if (r.emoji.name === "➡️") {
-                                    page += 1;
-                                    if (page + 1 > result.length) page = 0;
-                                    var editedEmbed = createEmbed(result, page);
-                                    embedMessage.edit(editedEmbed);
-                                }
-                            });
-                            collector.on("remove", (r) => {
-                                if (r.emoji.name === "⬅️") {
-                                    page -= 1;
-                                    if (page < 0) page = result.length - 1;
-                                    var editedEmbed = createEmbed(result, page);
-                                    embedMessage.edit(editedEmbed);
-                                } else if (r.emoji.name === "➡️") {
-                                    page += 1;
-                                    if (page + 1 > result.length) page = 0;
-                                    var editedEmbed = createEmbed(result, page);
-                                    embedMessage.edit(editedEmbed);
-                                }
-                            });
-                        });
+                        createEmbedFlip(result);
                     }
                 );
             }
@@ -207,117 +144,14 @@ module.exports = {
                 // get a list of data of multiple specific countries
                 api.countries({ country: args, allowNull: false }).then(
                     (result) => {
-                        console.log(result);
-                        var page = 0;
-                        let embed = createEmbed(result, page);
-                        message.channel.send(embed).then((embedMessage) => {
-                            embedMessage
-                                .react("⬅️")
-                                .then(embedMessage.react("➡️"));
-                            const filter = (reaction, user) =>
-                                ["⬅️", "➡️"].includes(reaction.emoji.name) &&
-                                !user.bot;
-                            const collector =
-                                embedMessage.createReactionCollector(filter, {
-                                    idle: 60000,
-                                    dispose: true,
-                                });
-                            collector.on("collect", (r) => {
-                                if (r.emoji.name === "⬅️") {
-                                    page -= 1;
-                                    if (page < 0) page = result.length - 1;
-                                    var editedEmbed = createEmbed(result, page);
-                                    embedMessage.edit(editedEmbed);
-                                } else if (r.emoji.name === "➡️") {
-                                    page += 1;
-                                    if (page + 1 > result.length) page = 0;
-                                    var editedEmbed = createEmbed(result, page);
-                                    embedMessage.edit(editedEmbed);
-                                }
-                            });
-                            collector.on("remove", (r) => {
-                                if (r.emoji.name === "⬅️") {
-                                    page -= 1;
-                                    if (page < 0) page = result.length - 1;
-                                    var editedEmbed = createEmbed(result, page);
-                                    embedMessage.edit(editedEmbed);
-                                } else if (r.emoji.name === "➡️") {
-                                    page += 1;
-                                    if (page + 1 > result.length) page = 0;
-                                    var editedEmbed = createEmbed(result, page);
-                                    embedMessage.edit(editedEmbed);
-                                }
-                            });
-                        });
+                        createEmbedFlip(result);
                     }
                 );
             } else {
                 // get a list of data of a single specific countries
                 api.countries({ country: args, allowNull: false }).then(
                     (result) => {
-                        console.log(result);
-                        const date = new Date(result.updated);
-                        const embed = new Discord.MessageEmbed()
-                            .setColor("RANDOM")
-                            .setTitle(`**${result.country} COVID-19 data**`)
-                            .setAuthor(
-                                "ChinoKafuu",
-                                "https://cdn.discordapp.com/avatars/781328218753859635/af716f0a9958679bdb17edfc0add53a6.png?size=256"
-                            )
-                            .addFields(
-                                {
-                                    name: "Total cases",
-                                    value: result.cases.toLocaleString(),
-                                    inline: true,
-                                },
-                                {
-                                    name: "Confirmed today",
-                                    value: result.todayCases.toLocaleString(),
-                                    inline: true,
-                                },
-                                {
-                                    name: "Total deaths",
-                                    value: result.deaths.toLocaleString(),
-                                    inline: true,
-                                },
-                                {
-                                    name: "Deaths today",
-                                    value: result.todayDeaths.toLocaleString(),
-                                    inline: true,
-                                },
-                                {
-                                    name: "Total recovered",
-                                    value: result.recovered.toLocaleString(),
-                                    inline: true,
-                                },
-                                {
-                                    name: "Recovered today",
-                                    value: result.todayRecovered.toLocaleString(),
-                                    inline: true,
-                                },
-                                {
-                                    name: "Active cases",
-                                    value: result.active.toLocaleString(),
-                                    inline: true,
-                                },
-                                {
-                                    name: "Critical cases",
-                                    value: result.critical.toLocaleString(),
-                                    inline: true,
-                                },
-                                {
-                                    name: "Population",
-                                    value: result.population.toLocaleString(),
-                                    inline: true,
-                                }
-                            )
-                            .setThumbnail(result.countryInfo.flag)
-                            .setFooter(
-                                `Requested by: ${
-                                    message.author.tag
-                                }\nData updated: ${date.toUTCString()}`,
-                                message.author.avatarURL()
-                            );
+                        let embed = createEmbed(result);
                         return message.channel.send(embed);
                     }
                 );
