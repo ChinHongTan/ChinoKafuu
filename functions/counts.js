684134bfa8672684684efaaf2e42c23159166d3c
Chin Hong Tan <chinhongtan1@gmail.com>
2021-07-10 20:25:20 +0800

    Update counts.js

--- functions/counts.js
+++ functions/counts.js
@@ -5,28 +5,35 @@ module.exports = {
 
         let rawData = fs.readFileSync("../data/countingData.json");
         let countingData = JSON.parse(rawData);
-        if (!Number(message.content)) return;
-        if (countingData.counter == "0" || !countingData.counter) {
-            if (message.content != "1") return;
+        if (!Number(message.content)) {
+            return;
+        }
+        if (countingData.counter === "0" || !countingData.counter) {
+            if (message.content !== "1") {
+                return;
+            }
             countingData.author = message.author.tag;
             countingData.counter = message.content;
             let data = JSON.stringify(countingData, null, 2);
             fs.writeFileSync("../data/countingData.json", data);
-            return message.react("✅");
+            message.react("✅");
+            return;
         }
-        if (message.author.tag != countingData.author) {
-            if (message.content != Number(countingData.counter) + 1) {
+        if (message.author.tag !== countingData.author) {
+            if (message.content !== Number(countingData.counter) + 1) {
                 countingData.counter = "0";
                 countingData.author = "";
                 let data = JSON.stringify(countingData, null, 2);
                 fs.writeFileSync("../data/countingData.json", data);
-                return message.react("❌");
+                message.react("❌");
+                return;
             } else {
                 countingData.counter = message.content;
                 countingData.author = message.author.tag;
                 let data = JSON.stringify(countingData, null, 2);
                 fs.writeFileSync("../data/countingData.json", data);
-                return message.react("✅");
+                message.react("✅");
+                return;
             }
         }
     },
