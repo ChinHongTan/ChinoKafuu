0610f218f55916cc812eaf9f8d2e71efca9ea1af
Chin Hong Tan <chinhongtan1@gmail.com>
2021-07-11 21:41:44 +0800

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,25 +4,35 @@ module.exports = {
     aliases: ["icon", "pfp"],
     description: "Send the url of an avatar.",
     execute(message) {
+        const Discord = require("discord.js");
         if (!message.mentions.users.size) {
-            const Discord = require("discord.js");
             const embed = new Discord.MessageEmbed()
                 .setTitle("__Your avatar__")
                 .setColor("RANDOM")
-                .setImage(`${message.author.displayAvatarURL({format: "png",dynamic: true})}`)
+                .setImage(
+                    `${message.author.displayAvatarURL({
+                        format: "png",
+                        dynamic: true,
+                        size: 1024
+                    })}`
+                );
             return message.channel.send(embed);
         }
 
         const avatarList = message.mentions.users.map((user) => {
             const embed = new Discord.MessageEmbed()
-                .setTitle(`__${user.username} avatar__`)
+                .setTitle(`__${user.username}'s avatar__`)
                 .setColor("RANDOM")
-                .setImage(`${user.displayAvatarURL({format: "png",dynamic: true})}`)
+                .setImage(
+                    `${user.displayAvatarURL({ format: "png", dynamic: true, size: 1024 })}`
+                );
             return embed;
         });
 
         // send the entire array of strings as a message
         // by default, discord.js will `.join()` the array with `\n`
-        message.channel.send(avatarList);
+        avatarList.forEach (embed => {
+            message.channel.send(embed);
+        })
     },
 };
