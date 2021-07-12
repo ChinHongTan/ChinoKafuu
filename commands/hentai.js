013f544cd6b2ca3e48f103fd214b749122fb6b24
Chin Hong Tan <chinhongtan1@gmail.com>
2021-07-12 19:08:36 +0800

    Update hentai.js

--- commands/hentai.js
+++ commands/hentai.js
@@ -6,13 +6,14 @@ module.exports = {
         const fs = require("fs");
         const Discord = require("discord.js");
 
-        if (!message.channel.nsfw)
+        if (!message.channel.nsfw) {
             return message.channel.send("This is not a nsfw channel!");
+        }
 
         const imageFiles = fs
             .readdirSync("./images")
             .filter((file) => file.endsWith(".json"));
-        pictures = new Object();
+        let pictures = new Object();
 
         for (const filename of imageFiles) {
             let rawdata = fs.readFileSync(`./images/${filename}`);
@@ -32,16 +33,15 @@ module.exports = {
         let imageInfo = images.attachments[random];
         let embed = new Discord.MessageEmbed()
             .setColor("#2d9af8")
-            .setTitle(`蘿莉圖！`)
+            .setTitle("死变态！")
             .setDescription(
                 `來源商：${images.author}\n[圖片鏈接](${messageurl})`
             )
             .setImage(imageInfo.url)
             .setFooter(
-                `蘿莉控的FBI避難所`,
+                "蘿莉控的FBI避難所",
                 "https://cdn.discordapp.com/icons/764839074228994069/5be3f532073fdae6a9d934e1c6f6a2b5.png?size=2048"
             );
         message.channel.send(embed);
-        fs.writeFileSync(`../data/` + filename, data);
     },
 };
