f026a7502e2bd174d11447deaedcc40601ddafa8
Chin Hong Tan <chinhongtan1@gmail.com>
2021-07-12 12:23:12 +0800

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -41,25 +41,25 @@ module.exports = {
                 message.channel.send(embed);
             });
         }
-        let user = message.guild.members.cache.find(member => member.user.id == args[0]);
+        let user = message.guild.members.cache.find(
+            (member) => member.user.id == args[0]
+        );
         if (user) {
             const embed = new Discord.MessageEmbed()
-            .setTitle(`__${user.displayName}'s avatar__`)
-            .setColor(user.displayHexColor)
-            .setImage(
-                `${user.user.displayAvatarURL({
-                    format: "png",
-                    dynamic: true,
-                    size: 2048,
-                })}`
-            );
+                .setTitle(`__${user.displayName}'s avatar__`)
+                .setColor(user.displayHexColor)
+                .setImage(
+                    `${user.user.displayAvatarURL({
+                        format: "png",
+                        dynamic: true,
+                        size: 2048,
+                    })}`
+                );
             return message.channel.send(embed);
         }
 
-        let keyword = message.content.substr(
-            message.content.indexOf(" ") + 1
-        );
-        let arr = message.guild.members.cache.map(member => {
+        let keyword = message.content.substr(message.content.indexOf(" ") + 1);
+        let arr = message.guild.members.cache.map((member) => {
             let memberInfo = {};
             memberInfo.nickname = member.nickname;
             memberInfo.username = member.user.username;
@@ -67,12 +67,19 @@ module.exports = {
             memberInfo.discriminator = member.user.discriminator;
             return memberInfo;
         });
-        let result = fuzzysort.go(keyword, arr, { keys: ['nickname', 'username', 'tag', 'discriminator'], limit: 1 });
+        let result = fuzzysort.go(keyword, arr, {
+            keys: ["nickname", "username", "tag", "discriminator"],
+            limit: 1,
+        });
         if (!result[0]) {
-            return message.channel.send(`Can't find a member matching \`${keyword}\`!`)
+            return message.channel.send(
+                `Can't find a member matching \`${keyword}\`!`
+            );
         }
-        let member = message.guild.members.cache.find(member => member.user.tag == result[0].obj.tag);
-        
+        let member = message.guild.members.cache.find(
+            (member) => member.user.tag == result[0].obj.tag
+        );
+
         const embed = new Discord.MessageEmbed()
             .setTitle(`__${member.displayName}'s avatar__`)
             .setColor(member.displayHexColor)
