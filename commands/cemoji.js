1f9382ce1332321268645ee446c031a156946f6a
Chin Hong Tan <chinhongtan1@gmail.com>
2021-07-17 19:24:22 +0800

    Update cemoji.js

--- commands/cemoji.js
+++ commands/cemoji.js
@@ -16,12 +16,10 @@ module.exports = {
         for (let [name, id] of Object.entries(emojiObj)) {
             console.log(name);
             if (name.startsWith('a:')) {
-                console.log(`https://cdn.discordapp.com/emojis/${id}.gif?v=1`)
                 message.guild.emojis.create(`https://cdn.discordapp.com/emojis/${id}.gif?v=1`, name.substring(2)).then(emoji => {
                     message.channel.send(`The emoji \`${emoji.name}\` ${emoji} was successfully added to the server!`);
                 });
             } else {
-                console.log(`https://cdn.discordapp.com/emojis/${id}.png?v=1`)
                 message.guild.emojis.create(`https://cdn.discordapp.com/emojis/${id}.png?v=1`, name.substring(1)).then(emoji => {
                     message.channel.send(`The emoji \`${emoji.name}\` ${emoji} was successfully added to the server!`);
             });
