5fb47167a0b25d81f0a14bf5e287b76ecc5d0c99
Chin Hong Tan <chinhongtan1@gmail.com>
2021-07-17 18:03:04 +0800

    new feature yay

--- commands/cemoji.js
+++ commands/cemoji.js
@@ -3,6 +3,29 @@ module.exports = {
     cooldown: 3,
     description: "Copy emoji!",
     execute(message, args) {
-        console.log(args);
+        const emojiName = message.content.match(/(?<=:.*:).+?(?=>)/g);
+        const emojiID = message.content.match(/(?<=<).+?(?=:\d)/g);
+        if (!emojiID || !emojiName) {
+            return message.channel.send("Please specify an emoji to add!");
+        }
+        let emojiObj = emojiID.reduce((obj, key, index) => {
+            obj[key] = emojiName[index];
+            return obj;
+        }, {});
+
+        for (let [name, id] of Object.entries(emojiObj)) {
+            console.log(name);
+            if (name.startsWith('a:')) {
+                console.log(`https://cdn.discordapp.com/emojis/${id}.gif?v=1`)
+                message.guild.emojis.create(`https://cdn.discordapp.com/emojis/${id}.gif?v=1`, name.substring(2)).then(emoji => {
+                    message.channel.send(`The emoji \`${emoji.name}\` ${emoji} was successfully added to the server!`);
+                });
+            } else {
+                console.log(`https://cdn.discordapp.com/emojis/${id}.png?v=1`)
+                message.guild.emojis.create(`https://cdn.discordapp.com/emojis/${id}.png?v=1`, name.substring(1)).then(emoji => {
+                    message.channel.send(`The emoji \`${emoji.name}\` ${emoji} was successfully added to the server!`);
+            });
+            }
+        }
     },
 };
