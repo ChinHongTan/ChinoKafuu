f524e09ddb9d8e4ab061571490df57466279e2d4
Chin Hong Tan <chinhongtan1@gmail.com>
2021-07-23 21:05:21 +0800

    code restructure

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -8,13 +8,15 @@ module.exports = {
         let queue = queueData.queue;
         let serverQueue = queue.get(message.guild.id);
         const Discord = require("discord.js");
+        const DynamicEmbed = require("../../functions/dynamicEmbed");
+        let dynamicEmbed = new DynamicEmbed();
         const array_chunks = (array, chunk_size) => Array(Math.ceil(array.length / chunk_size)).fill().map((_, index) => index * chunk_size).map(begin => array.slice(begin, begin + chunk_size));
 
         /**
-                 * Create an Discord embed message
-                 * @param {object} result - The result from the API.
-                 * @returns {object} Discord embed
-                 */
+         * Create an Discord embed message
+         * @param {object} result - The result from the API.
+         * @returns {object} Discord embed
+         */
          function createEmbed(smallChunk) {
             let printQueue = "";
             smallChunk.forEach((item) => {
@@ -34,57 +36,6 @@ module.exports = {
                     `**Now playing**\n[${serverQueue.songs[0].title}](${serverQueue.songs[0].url})\n\n**Queued Songs**\n${printQueue}${serverQueue.songs.length - 1} songs in queue`
                 );
             return embed;
-        }
-
-        /**
-         * Function to update embed message after a user had reacted
-         * @param {object} r - Reaction from the user
-         * @param {number} page - Which result to be displayed
-         * @param {object} result - The result from the API.
-         * @param {object} embedMessage - Discord embed message.
-         * @returns {number} Page
-         */
-        function updateEmbed(r, page, arrayChunk, embedMessage) {
-            let editedEmbed;
-            switch (r.emoji.name) {
-                case "⬅️":
-                    page -= 1;
-                    if (page < 0) page = arrayChunk.length - 1;
-                    editedEmbed = createEmbed(arrayChunk[page]);
-                    embedMessage.edit(editedEmbed);
-                    break;
-                case "➡️":
-                    page += 1;
-                    if (page + 1 > arrayChunk.length) page = 0;
-                    editedEmbed = createEmbed(arrayChunk[page]);
-                    embedMessage.edit(editedEmbed);
-                    break;
-            }
-            return page;
-        }
-
-        /**
-        * Creates and sends a reactable message
-        * @param {object} result - The result from the API.
-        */
-        function createEmbedFlip(arrayChunk) {
-            let page = 0;
-            let embed = createEmbed(arrayChunk[page]);
-            message.channel.send(embed).then((embedMessage) => {
-                embedMessage.react("⬅️").then(embedMessage.react("➡️"));
-                const filter = (reaction, user) =>
-                    ["⬅️", "➡️"].includes(reaction.emoji.name) && !user.bot;
-                const collector = embedMessage.createReactionCollector(filter, {
-                    idle: 60000,
-                    dispose: true,
-                });
-                collector.on("collect", (r) => {
-                    page = updateEmbed(r, page, arrayChunk, embedMessage);
-                });
-                collector.on("remove", (r) => {
-                    page = updateEmbed(r, page, arrayChunk, embedMessage);
-                });
-            });
         }
 
         function format(duration) {
@@ -112,7 +63,7 @@ module.exports = {
             })
             let arrayChunk = array_chunks(songQueue, 10);
             if (songQueue.length > 10) {
-                createEmbedFlip(arrayChunk);
+                dynamicEmbed.createEmbedFlip(message, arrayChunk, ["⬅️", "➡️"], createEmbed);
             } else {
                 let printQueue = "";
                 songQueue.forEach((item, index) => {
