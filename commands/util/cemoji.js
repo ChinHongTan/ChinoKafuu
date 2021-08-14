9d847628b2d88d13f078022656b6b59b0d161fd4
Chin Hong Tan <chinhongtan1@gmail.com>
2021-08-14 12:26:29 +0800

    fixing cemoji fail bug

--- commands/util/cemoji.js
+++ commands/util/cemoji.js
@@ -3,15 +3,16 @@ module.exports = {
     cooldown: 3,
     description: {"en_US" : "Copy emoji!", "zh_CN" : "复制表情!"},
     async execute(message, _args, language) {
-        const emojiName = message.content.match(/(?<=:.*:).+?(?=>)/g);
-        const emojiID = message.content.match(/(?<=<).+?(?=:\d)/g);
+        const emojiID = message.content.match(/(?<=:.*:).+?(?=>)/g);
+        const emojiName = message.content.match(/(?<=<).+?(?=:\d+>)/g);
         if (!emojiID || !emojiName) return message.channel.send(language.noEmoji);
-        let emojiObj = emojiID.reduce((obj, key, index) => {
-            obj[key] = emojiName[index];
+        let emojiObj = emojiName.reduce((obj, key, index) => {
+            obj[key] = emojiID[index];
             return obj;
         }, {});
 
         for (let [name, id] of Object.entries(emojiObj)) {
+            console.log(name);
             if (name.startsWith("a:")) {
                 let emoji = await message.guild.emojis.create(`https://cdn.discordapp.com/emojis/${id}.gif?v=1`, name.substring(2))
                 message.channel.send(language.addSuccess.replace("${emoji.name}", emoji.name).replace("${emoji}", emoji));
