63506db28aaaf550ab2e3a0adc90ac2748db0eb5
Chin Hong Tan <chinhongtan1@gmail.com>
2021-08-21 22:18:02 +0800

    new command prototypes, not useable, still in development

---
+++ commands/music/lyric.js
@@ -0,0 +1,65 @@
+module.exports = {
+    name: "lyric",
+    guildOnly: true,
+    aliases: ["ly"],
+    description: "Loop the currently played song!",
+    async execute(message, args, language) {
+        const queueData = require("../../data/queueData");
+        let queue = queueData.queue;
+        let serverQueue = queue.get(message.guild.id);
+        const lyricsFinder = require("lyrics-finder");
+        const solenolyrics= require("solenolyrics");
+        const Genius = require("genius-lyrics");
+        const Client = new Genius.Client();
+
+        if (!message.member.voice.channel) {
+            return message.channel.send(language.notInVC);
+        }
+        async function searchLyrics(keyword) {
+            let msg = await message.channel.send({
+                embed: {
+                    description: `:mag: | Searching lyrics for ${keyword}...`,
+                    color: "BLUE"
+                }
+            });
+            let lyrics = await lyricsFinder(keyword).catch((err) => console.error(err));
+            console.log("----");
+            console.log(lyrics);
+            if (!lyrics) {
+                let searches = await Client.songs.search(keyword);
+                if (searches) lyrics = await searches[0].lyrics();
+            }
+            console.log("----");
+            console.log(lyrics);
+            if (!lyrics) lyrics = await solenolyrics.requestLyricsFor(encodeURLComponent(keyword)) ?? undefined;
+            console.log("----");
+            console.log(lyrics);
+            if (!lyrics) return msg.edit({
+                embed: {
+                    title: `ERROR!`,
+                    description: `:x: | No lyrics found for \`${keyword}\`!`,
+                    color: "RED"
+                }
+            });
+            msg.edit({
+                embed: {
+                    title: `Lyric for \`${keyword}\``,
+                    description: lyrics,
+                    color: "YELLOW"
+                }
+            });
+        }
+
+        if (serverQueue) {
+            let songTitle = serverQueue.songs[0].title;
+            let keyword;
+            if (args[0]) keyword = message.content.substr(message.content.indexOf(" ") + 1);
+            await searchLyrics(keyword ?? songTitle);
+        } else {
+            let keyword;
+            if (args[0]) keyword = message.content?.substr(message.content?.indexOf(" ") + 1);
+            if (keyword) return await searchLyrics(keyword);
+            return message.channel.send(language.noSong);
+        }
+    },
+};
