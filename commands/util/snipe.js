4170cd5c24aa3ec5a3f00f10c356f0eff7cde055
Chin Hong Tan <chinhongtan1@gmail.com>
2021-08-23 14:21:21 +0800

    slash command support

--- commands/util/snipe.js
+++ commands/util/snipe.js
@@ -1,9 +1,10 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
     name: "snipe",
     guildOnly: true,
     description: true,
     async execute(message, args, language) {
-        const Discord = require("discord.js");
+        const { MessageEmbed } = require("discord.js");
         const collection = message.client.snipeCollection;
 
         let snipeWithGuild = await collection.findOne({ id: message.guild.id });
@@ -22,12 +23,46 @@ module.exports = {
 
         let image = msg.attachments;
 
-        let embed = new Discord.MessageEmbed()
+        let embed = new MessageEmbed()
             .setColor("RANDOM")
             .setAuthor(msg.author, msg.authorAvatar)
             .setDescription(msg.content)
             .setFooter(msg.timestamp)
             .setImage(image);
         return message.channel.send(embed);
+    },
+    slashCommand: {
+        data: new SlashCommandBuilder()
+            .setName("snipe")
+            .setDescription("Snipe a message")
+            .addIntegerOption(option => option.setName("number").setDescription("message to snipe")),
+        async execute(interaction, language) {
+            const { MessageEmbed } = require("discord.js");
+            const collection = interaction.client.snipeCollection;
+
+            let snipeWithGuild = await collection.findOne({ id: interaction.guild.id });
+            let snipes;
+
+            if (snipeWithGuild) {
+                snipes = snipeWithGuild.snipes;
+            } else {
+                return interaction.reply(language.noSnipe);
+            }
+            let arg = interaction.options.getInteger('number') ?? 1;
+
+            if (Number(arg) > 10) return interaction.reply(language.exceed10);
+            let msg = snipes?.[Number(arg) - 1];
+            if (!msg) return interaction.reply(language.invalidSnipe);
+
+            let image = msg.attachments;
+
+            let embed = new MessageEmbed()
+                .setColor("RANDOM")
+                .setAuthor(msg.author, msg.authorAvatar)
+                .setDescription(msg.content)
+                .setFooter(msg.timestamp)
+                .setImage(image);
+            return interaction.reply({embeds: [embed]});
+        },
     },
 };
