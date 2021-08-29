dec7aed25e13f79138bcc93a815766e134378c5a
Chin Hong Tan <chinhongtan1@gmail.com>
2021-08-29 19:59:34 +0800

    updated reply function to accept MessageEmbed object

--- functions/commandReply.ts
+++ functions/commandReply.ts
@@ -5,23 +5,25 @@ class CommandReply {
         this.command = command;
     }
     // reply to a user command
-    reply(command: Discord.Message | Discord.CommandInteraction, response: string, color: Discord.ColorResolvable) {
+    reply(command: Discord.Message | Discord.CommandInteraction, response: string | Discord.MessageEmbed, color: Discord.ColorResolvable) {
+        if (response instanceof Discord.MessageEmbed) {
+            if (command instanceof Discord.Message) return command.channel.send({ embeds: [response] });
+            return command.reply({ embeds: [response] });
+        }
         if (command instanceof Discord.Message) {
             return command.channel.send({
                 embeds: [{
                     description: response,
                     color: color
                 }]
-            })
+            });
         }
-        if (command instanceof Discord.CommandInteraction) {
-            return command.reply({
-                embeds: [{
-                    description: response,
-                    color: color
-                }]
-            })
-        }
+        return command.reply({
+            embeds: [{
+                description: response,
+                color: color
+            }]
+        });
     }
 }
 module.exports = CommandReply;
