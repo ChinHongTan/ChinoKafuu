7d00b641401367ef532174140e65dd8948d15f04
Chin Hong Tan <chinhongtan1@gmail.com>
2021-08-29 18:38:08 +0800

    v13 update

--- commands/music/loop-queue.js
+++ commands/music/loop-queue.js
@@ -1,3 +1,6 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const CommandReply = require('../../functions/commandReply.js');
+const commandReply = new CommandReply();
 module.exports = {
     name: 'loop-queue',
     guildOnly: true,
@@ -9,14 +12,35 @@ module.exports = {
         const serverQueue = queue.get(message.guild.id);
 
         if (!message.member.voice.channel) {
-            return message.channel.send(language.notInVC);
+            return commandReply.reply(message, language.notInVC, 'RED');
         }
 
         if (serverQueue) {
             serverQueue.loopQueue = !serverQueue.loopQueue;
             if (serverQueue.loop) serverQueue.loop = false;
-            return message.channel.send(serverQueue.loopQueue ? 'Loop queue on!' : 'Loop queue off!');
+            return commandReply.reply(message, serverQueue.loopQueue ? 'Loop queue on!' : 'Loop queue off!', 'GREEN');
         }
-        return message.channel.send(language.noSong);
+        return commandReply.reply(message, language.noSong, 'RED');
+    },
+    slashCommand: {
+        data: new SlashCommandBuilder()
+            .setName('loop-queue')
+            .setDescription('Loop the currently played queue!'),
+        async execute(interaction, language) {
+            const queueData = require('../../data/queueData');
+            const { queue } = queueData;
+            const serverQueue = queue.get(interaction.guild.id);
+
+            if (!interaction.member.voice.channel) {
+                return commandReply.reply(interaction, language.notInVC, 'RED');
+            }
+
+            if (serverQueue) {
+                serverQueue.loopQueue = !serverQueue.loopQueue;
+                if (serverQueue.loop) serverQueue.loop = false;
+                return commandReply.reply(interaction, serverQueue.loopQueue ? 'Loop queue on!' : 'Loop queue off!', 'GREEN');
+            }
+            return commandReply.reply(interaction, language.noSong, 'RED');
+        },
     },
 };
--- commands/music/loop.js
+++ commands/music/loop.js
@@ -1,3 +1,6 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const CommandReply = require('../../functions/commandReply.js');
+const commandReply = new CommandReply();
 module.exports = {
     name: 'loop',
     guildOnly: true,
@@ -8,14 +11,35 @@ module.exports = {
         const serverQueue = queue.get(message.guild.id);
 
         if (!message.member.voice.channel) {
-            return message.channel.send(language.notInVC);
+            return commandReply.reply(message, language.notInVC, 'RED');
         }
 
         if (serverQueue) {
             serverQueue.loop = !serverQueue.loop;
             if (serverQueue.loopQueue) serverQueue.loopQueue = false;
-            return message.channel.send(serverQueue.loop ? 'Loop mode on!' : 'Loop mode off!');
+            return commandReply.reply(message, serverQueue.loop ? 'Loop mode on!' : 'Loop mode off!', 'GREEN');
         }
-        return message.channel.send(language.noSong);
+        return commandReply.reply(message, language.noSong, 'RED');
+    },
+    slashCommand: {
+        data: new SlashCommandBuilder()
+            .setName('loop')
+            .setDescription('Loop the currently played song!'),
+        async execute(interaction, language) {
+            const queueData = require('../../data/queueData');
+            const { queue } = queueData;
+            const serverQueue = queue.get(interaction.guild.id);
+
+            if (!interaction.member.voice.channel) {
+                return commandReply.reply(interaction, language.notInVC, 'RED');
+            }
+
+            if (serverQueue) {
+                serverQueue.loop = !serverQueue.loop;
+                if (serverQueue.loopQueue) serverQueue.loopQueue = false;
+                return commandReply.reply(interaction, serverQueue.loopQueue ? 'Loop mode on!' : 'Loop mode off!', 'GREEN');
+            }
+            return commandReply.reply(interaction, language.noSong, 'RED');
+        },
     },
 };
