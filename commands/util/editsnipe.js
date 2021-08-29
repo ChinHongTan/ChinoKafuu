26a86cf65eb03178f7539c53fde97ba553196547
Chin Hong Tan <chinhongtan1@gmail.com>
2021-08-29 19:59:09 +0800

    snipe command tidy up

--- commands/util/editsnipe.js
+++ commands/util/editsnipe.js
@@ -1,31 +1,34 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const CommandReply = require('../../functions/commandReply.js');
 const commandReply = new CommandReply();
+function esnipe(command, args, language) {
+    const { MessageEmbed } = require('discord.js');
+    const collection = command.client.editSnipeCollection;
+
+    const editSnipesWithGuild = await collection.findOne({ id: command.guild.id });
+    const arg = args[0] ?? 1;
+
+    if (editSnipesWithGuild) {
+        const editsnipes = editSnipesWithGuild.editSnipe;
+        if (Number(arg) > 10) return commandReply.reply(command, language.exceed10, 'RED');
+        const msg = editsnipes?.[Number(arg) - 1];
+        if (!msg) return commandReply.reply(command, language.invalidSnipe, 'RED');
+        const embed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setAuthor(msg.author, msg.authorAvatar)
+            .setDescription(msg.content)
+            .setFooter(msg.timestamp);
+        return commandReply.reply(command, embed);
+    }
+    return commandReply.reply(command, language.noSnipe, 'RED');
+}
 module.exports = {
     name: 'editsnipe',
     aliases: ['esnipe'],
     guildOnly: true,
     description: true,
     async execute(message, args, language) {
-        const { MessageEmbed } = require('discord.js');
-        const collection = message.client.editSnipeCollection;
-
-        const editSnipesWithGuild = await collection.findOne({ id: message.guild.id });
-        const arg = args[0] ?? 1;
-
-        if (editSnipesWithGuild) {
-            const editsnipes = editSnipesWithGuild.editSnipe;
-            if (Number(arg) > 10) return commandReply.reply(message, language.exceed10, 'RED');
-            const msg = editsnipes?.[Number(arg) - 1];
-            if (!msg) return commandReply.reply(message, language.invalidSnipe, 'RED');
-            const embed = new MessageEmbed()
-                .setColor('RANDOM')
-                .setAuthor(msg.author, msg.authorAvatar)
-                .setDescription(msg.content)
-                .setFooter(msg.timestamp);
-            return message.channel.send({ embeds: [embed] });
-        }
-        return commandReply.reply(message, language.noSnipe, 'RED');
+        esnipe(message, args, language);
     },
     slashCommand: {
         data: new SlashCommandBuilder()
@@ -33,25 +36,7 @@ module.exports = {
             .setDescription('Snipe an edited message')
             .addIntegerOption((option) => option.setName('number').setDescription('message to snipe')),
         async execute(interaction, language) {
-            const { MessageEmbed } = require('discord.js');
-            const collection = interaction.client.editSnipeCollection;
-
-            const editSnipesWithGuild = await collection.findOne({ id: interaction.guild.id });
-            const arg = interaction.options.getInteger('number') ?? 1;
-
-            if (editSnipesWithGuild) {
-                const editsnipes = editSnipesWithGuild.editSnipe;
-                if (Number(arg) > 10) return commandReply.reply(interaction, language.exceed10, 'RED');
-                const msg = editsnipes?.[Number(arg) - 1];
-                if (!msg) return commandReply.reply(interaction, language.invalidSnipe, 'RED');
-                const embed = new MessageEmbed()
-                    .setColor('RANDOM')
-                    .setAuthor(msg.author, msg.authorAvatar)
-                    .setDescription(msg.content)
-                    .setFooter(msg.timestamp);
-                return interaction.reply({ embeds: [embed] });
-            }
-            return commandReply.reply(interaction, language.noSnipe, 'RED');
+            esnipe(interaction, interaction.options.getInteger('number') ?? 1, language);
         },
     },
 };
--- commands/util/snipe.js
+++ commands/util/snipe.js
@@ -1,38 +1,41 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const CommandReply = require('../../functions/commandReply.js');
 const commandReply = new CommandReply();
+function snipe(command, args, language) {
+    const { MessageEmbed } = require('discord.js');
+    const collection = command.client.snipeCollection;
+
+    const snipeWithGuild = await collection.findOne({ id: command.guild.id });
+    let snipes;
+
+    if (snipeWithGuild) {
+        snipes = snipeWithGuild.snipes;
+    }
+    else {
+        return commandReply.reply(command, language.noSnipe, 'RED');
+    }
+    const arg = args[0] ?? 1;
+
+    if (Number(arg) > 10) return commandReply.reply(command, language.exceed10, 'RED');
+    const msg = snipes?.[Number(arg) - 1];
+    if (!msg) return commandReply.reply(command, language.invalidSnipe, 'RED');
+
+    const image = msg.attachments;
+
+    const embed = new MessageEmbed()
+        .setColor('RANDOM')
+        .setAuthor(msg.author, msg.authorAvatar)
+        .setDescription(msg.content)
+        .setFooter(msg.timestamp)
+        .setImage(image);
+    return commandReply.reply(command, embed);
+}
 module.exports = {
     name: 'snipe',
     guildOnly: true,
     description: true,
     async execute(message, args, language) {
-        const { MessageEmbed } = require('discord.js');
-        const collection = message.client.snipeCollection;
-
-        const snipeWithGuild = await collection.findOne({ id: message.guild.id });
-        let snipes;
-
-        if (snipeWithGuild) {
-            snipes = snipeWithGuild.snipes;
-        }
-        else {
-            return commandReply.reply(message, language.noSnipe, 'RED');
-        }
-        const arg = args[0] ?? 1;
-
-        if (Number(arg) > 10) return commandReply.reply(message, language.exceed10, 'RED');
-        const msg = snipes?.[Number(arg) - 1];
-        if (!msg) return commandReply.reply(message, language.invalidSnipe, 'RED');
-
-        const image = msg.attachments;
-
-        const embed = new MessageEmbed()
-            .setColor('RANDOM')
-            .setAuthor(msg.author, msg.authorAvatar)
-            .setDescription(msg.content)
-            .setFooter(msg.timestamp)
-            .setImage(image);
-        return message.channel.send({ embeds: [embed] });
+        snipe(message, args, language);
     },
     slashCommand: {
         data: new SlashCommandBuilder()
@@ -40,33 +43,7 @@ module.exports = {
             .setDescription('Snipe a message')
             .addIntegerOption((option) => option.setName('number').setDescription('message to snipe')),
         async execute(interaction, language) {
-            const { MessageEmbed } = require('discord.js');
-            const collection = interaction.client.snipeCollection;
-
-            const snipeWithGuild = await collection.findOne({ id: interaction.guild.id });
-            let snipes;
-
-            if (snipeWithGuild) {
-                snipes = snipeWithGuild.snipes;
-            }
-            else {
-                return commandReply.reply(interaction, language.noSnipe, 'RED');
-            }
-            const arg = interaction.options.getInteger('number') ?? 1;
-
-            if (arg > 10) return commandReply.reply(interaction, language.exceed10, 'RED');
-            const msg = snipes?.[arg - 1];
-            if (!msg) return commandReply.reply(interaction, language.invalidSnipe, 'RED');
-
-            const image = msg.attachments;
-
-            const embed = new MessageEmbed()
-                .setColor('RANDOM')
-                .setAuthor(msg.author, msg.authorAvatar)
-                .setDescription(msg.content)
-                .setFooter(msg.timestamp)
-                .setImage(image);
-            return interaction.reply({ embeds: [embed] });
+            snipe(interaction, interaction.options.getInteger('number') ?? 1, language);
         },
     },
 };
