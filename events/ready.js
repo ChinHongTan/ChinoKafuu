ee71b730c4c8c4ccb64a2ec04e341a673f980d60
Chin Hong Tan <chinhongtan1@gmail.com>
2021-08-30 10:15:02 +0800

    added translation for slash command for each guild

--- events/ready.js
+++ events/ready.js
@@ -10,37 +10,44 @@ module.exports = {
         client.user.setPresence({
             activity: { name: 'c!help', type: 'LISTENING' },
         });
-        const commands = [];
-        const commandFolders = fs.readdirSync('./commands');
-        for (const folder of commandFolders) {
-            const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith('.js'));
-            for (const file of commandFiles) {
-                const command = require(`../commands/${folder}/${file}`);
-                if (command.slashCommand) {
-                    const { data } = command.slashCommand;
-                    if (!data.description) data.setDescription('none');
-                    commands.push(command.slashCommand.data.toJSON());
-                }
-            }
-        }
-        const guilds = [];
-        client.guilds.cache.each((guild) => guilds.push(guild.id));
         const rest = new REST({ version: '9' }).setToken(token);
 
-        guilds.forEach((id) => {
-            (async () => {
-                try {
-                    await rest.put(
-                        Routes.applicationGuildCommands(clientId, id),
-                        { body: commands },
-                    );
+        const guilds = [];
+        client.guilds.cache.each((guild) => guilds.push(guild.id));
 
-                    console.log('Successfully registered application commands.');
+        for (const id of guilds) {
+            const collection = client.guildOptions;
+            const rawData = await collection.findOne({ id });
+            const guildOption = rawData ?? {
+                id,
+                options: { language: 'en_US' },
+            };
+            const language = client.language[guildOption.options.language];
+            const commands = [];
+            const commandFolders = fs.readdirSync('./commands');
+            for (const folder of commandFolders) {
+                const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith('.js'));
+                for (const file of commandFiles) {
+                    const command = require(`../commands/${folder}/${file}`);
+                    if (command.slashCommand) {
+                        const { data } = command.slashCommand;
+                        data.setName(command.name);
+                        data.setDescription(language[command.name] ?? 'none');
+                        commands.push(command.slashCommand.data.toJSON());
+                    }
                 }
-                catch (error) {
-                    console.error(error);
-                }
-            })();
-        });
+            }
+            try {
+                await rest.put(
+                    Routes.applicationGuildCommands(clientId, id),
+                    { body: commands },
+                );
+
+                console.log('Successfully registered application commands.');
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
     },
 };
