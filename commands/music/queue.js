b49bc6707400ff173588cb8285fe00feb2f75948
Chin Hong Tan <chinhongtan1@gmail.com>
2021-08-31 18:30:26 +0800

    v13 slash command update

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,52 +1,64 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const CommandReply = require('../../functions/commandReply.js');
+const commandReply = new CommandReply();
+function queueFunc(command, language) {
+    const queueData = require('../../data/queueData');
+    const { queue } = queueData;
+    const serverQueue = queue.get(command.guild.id);
+    const { format } = require('../../functions/musicFunctions');
+    const Discord = require('discord.js');
+    const DynamicEmbed = require('../../functions/dynamicEmbed');
+    const dynamicEmbed = new DynamicEmbed();
+    const array_chunks = (array, chunkSize) => Array(Math.ceil(array.length / chunkSize)).fill().map((_, index) => index * chunkSize).map((begin) => array.slice(begin, begin + chunkSize));
+
+    /**
+     * Create an Discord embed message
+     * @param {object} smallChunk - Song queue split by 10 songs
+     * @return {object} Discord embed
+     */
+    function createEmbed(smallChunk) {
+        const arr = smallChunk.map((item) => `${item.index}.[${item.title}](${item.url}) | ${format(item.duration)}`);
+        const printQueue = arr.join('\n\n');
+        return new Discord.MessageEmbed()
+            .setColor('#ff0000')
+            .setTitle(language.queueTitle)
+            .setDescription(language.queueBody.replace('${serverQueue.songs[0].title}', serverQueue.songs[0].title).replace('${serverQueue.songs[0].url}', serverQueue.songs[0].url).replace('${printQueue}', printQueue).replace('${serverQueue.songs.length - 1}', serverQueue.songs.length - 1));
+    }
+
+    if (!command.member.voice.channel) {
+        return commandReply.reply(command, language.notInVC, 'RED');
+    }
+
+    if (serverQueue) {
+        const songQueue = serverQueue.songs.slice(1);
+        songQueue.forEach((item, index) => {
+            item.index = index + 1;
+        });
+        const arrayChunk = array_chunks(songQueue, 10);
+        if (songQueue.length > 10) {
+            dynamicEmbed.createEmbedFlip(command, arrayChunk, ['⬅️', '➡️'], createEmbed);
+        }
+        else {
+            const embed = createEmbed(songQueue);
+            return commandReply.reply(command, embed);
+        }
+    }
+    else {
+        return commandReply.reply(command, language.noSong, 'RED');
+    }
+}
 module.exports = {
     name: 'queue',
     guildOnly: true,
     aliases: ['q'],
     description: true,
     execute(message, _args, language) {
-        const queueData = require('../../data/queueData');
-        const { queue } = queueData;
-        const serverQueue = queue.get(message.guild.id);
-        const { format } = require('../../functions/musicFunctions');
-        const Discord = require('discord.js');
-        const DynamicEmbed = require('../../functions/dynamicEmbed');
-        const dynamicEmbed = new DynamicEmbed();
-        const array_chunks = (array, chunkSize) => Array(Math.ceil(array.length / chunkSize)).fill().map((_, index) => index * chunkSize).map((begin) => array.slice(begin, begin + chunkSize));
-
-        /**
-         * Create an Discord embed message
-         * @param {object} smallChunk - Song queue split by 10 songs
-         * @return {object} Discord embed
-         */
-        function createEmbed(smallChunk) {
-            const arr = smallChunk.map((item) => `${item.index}.[${item.title}](${item.url}) | ${format(item.duration)}`);
-            const printQueue = arr.join('\n\n');
-            return new Discord.MessageEmbed()
-                .setColor('#ff0000')
-                .setTitle(language.queueTitle)
-                .setDescription(language.queueBody.replace('${serverQueue.songs[0].title}', serverQueue.songs[0].title).replace('${serverQueue.songs[0].url}', serverQueue.songs[0].url).replace('${printQueue}', printQueue).replace('${serverQueue.songs.length - 1}', serverQueue.songs.length - 1));
-        }
-
-        if (!message.member.voice.channel) {
-            return message.channel.send(language.notInVC);
-        }
-
-        if (serverQueue) {
-            const songQueue = serverQueue.songs.slice(1);
-            songQueue.forEach((item, index) => {
-                item.index = index + 1;
-            });
-            const arrayChunk = array_chunks(songQueue, 10);
-            if (songQueue.length > 10) {
-                dynamicEmbed.createEmbedFlip(message, arrayChunk, ['⬅️', '➡️'], createEmbed);
-            }
-            else {
-                const embed = createEmbed(songQueue);
-                return message.channel.send(embed);
-            }
-        }
-        else {
-            return message.channel.send(language.noSong);
-        }
+        queueFunc(message, language);
+    },
+    slashCommand: {
+        data: new SlashCommandBuilder(),
+        async execute(interaction, language) {
+            await queueFunc(interaction, language);
+        },
     },
 };
