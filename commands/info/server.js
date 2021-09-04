848fd8d4d9fc3e375ddfad49ac4983b4bc9d278f
Chin Hong Tan <chinhongtan1@gmail.com>
2021-09-04 21:35:34 +0800

    v13 update

--- commands/info/server.js
+++ commands/info/server.js
@@ -1,3 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const CommandReply = require('../../functions/commandReply.js');
+const { MessageEmbed } = require("discord.js");
+const commandReply = new CommandReply();
+function server(command) {
+    const embed = new MessageEmbed()
+        .setTitle('Server Info')
+        .setThumbnail(command.guild.iconURL())
+        .setDescription(`Information about ${command.guild.name}`)
+        .setColor('BLUE')
+        .setAuthor(`${command.guild.name} Info`, command.guild.iconURL())
+        .addFields(
+            { name: 'Server name', value: command.guild.name, inline: true },
+            { name: 'Server owner', value: command.guild.owner, inline: true },
+            { name: 'Member count', value: command.guild.memberCount, inline: true },
+            { name: 'Region', value: command.guild.region, inline: true },
+            { name: 'Hightst role', value: command.guild.roles.highest, inline: true },
+            { name: 'Server creation', value: command.guild.createdAt, inline: true },
+            { name: 'Channels count', value: command.guild.channels.cache.size, inline: true },
+        )
+        .setFooter('ChinoKafuu | Server Info', command.client.user.displayAvatarURL());
+    commandReply.reply(command, embed);
+}
 module.exports = {
     name: 'server',
     aliases: ['server-info'],
@@ -5,23 +28,12 @@ module.exports = {
     guildOnly: true,
     cooldown: 5,
     execute(message) {
-        const Discord = require('discord.js');
-        const embed = new Discord.MessageEmbed()
-            .setTitle('Server Info')
-            .setThumbnail(message.guild.iconURL())
-            .setDescription(`Information about ${message.guild.name}`)
-            .setColor('BLUE')
-            .setAuthor(`${message.guild.name} Info`, message.guild.iconURL())
-            .addFields(
-                { name: 'Server name', value: message.guild.name, inline: true },
-                { name: 'Server owner', value: message.guild.owner, inline: true },
-                { name: 'Member count', value: message.guild.memberCount, inline: true },
-                { name: 'Region', value: message.guild.region, inline: true },
-                { name: 'Hightst role', value: message.guild.roles.highest, inline: true },
-                { name: 'Server creation', value: message.guild.createdAt, inline: true },
-                { name: 'Channels count', value: message.guild.channels.cache.size, inline: true },
-            )
-            .setFooter('ChinoKafuu | Server Info', message.client.user.displayAvatarURL());
-        message.channel.send(embed);
+        server(message);
+    },
+    slashCommand: {
+        data: new SlashCommandBuilder(),
+        async execute(interaction, language) {
+            await server(interaction, language);
+        },
     },
 };
