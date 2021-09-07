531644f2979a9295cc97c7182d8a9ef932b340c4
Chin Hong Tan <chinhongtan1@gmail.com>
2021-09-07 17:16:43 +0800

    Update avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,16 +1,14 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const CommandReply = require('../../functions/commandReply.js');
 const FuzzySort = require('../../functions/fuzzysort.js');
-const Discord = require('discord.js');
 const commandReply = new CommandReply();
-
-// const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 function avatar(command, args, language) {
     const fuzzysort = new FuzzySort(command);
     if (args.length < 1) {
         // display author's avatar
-        const embed = new Discord.MessageEmbed()
+        const embed = new MessageEmbed()
             .setTitle(language.yourAvatar)
             .setColor('RANDOM')
             .setImage(`${command.author.displayAvatarURL({
@@ -25,7 +23,7 @@ function avatar(command, args, language) {
     const user = command.guild.members.cache.find((member) => member.user.id === args[0]);
     // if id exists
     if (user) {
-        const embed = new Discord.MessageEmbed()
+        const embed = new MessageEmbed()
             .setTitle(language.memberAvatar.replace('${user.displayName}', user.displayName))
             .setColor(user.displayHexColor)
             .setImage(
@@ -45,7 +43,7 @@ function avatar(command, args, language) {
         return commandReply.reply(command, language.noMember.replace('${keyword}', keyword), 'RED');
     }
 
-    const embed = new Discord.MessageEmbed()
+    const embed = new MessageEmbed()
         .setTitle(language.memberAvatar.replace('${user.displayName}', member.displayName))
         .setColor(member.displayHexColor)
         .setImage(`${member.user.displayAvatarURL({
@@ -65,7 +63,7 @@ module.exports = {
         if (message.mentions.users.size) {
             // display all user's avatars mentioned by the author
             const avatarList = message.mentions.users.map((user) => {
-                return new Discord.MessageEmbed()
+                return new MessageEmbed()
                     .setTitle(language.userAvatar.replace('${user.username}', user.username))
                     .setColor('RANDOM')
                     .setImage(`${user.displayAvatarURL({
@@ -89,7 +87,7 @@ module.exports = {
             .addUserOption((option) => option.setName('member').setDescription('member\'s avatar')),
         async execute(interaction, language) {
             const user = interaction.options.getMember('member') ?? interaction.member;
-            const embed = new Discord.MessageEmbed()
+            const embed = new MessageEmbed()
                 .setTitle(language.memberAvatar.replace('${user.displayName}', user.displayName))
                 .setColor(user.displayHexColor)
                 .setImage(
