13535040796b332a0e8c53b3a7602d2e0b1a0459
Chin Hong Tan <chinhongtan1@gmail.com>
2021-09-08 11:47:05 +0800

    Update google.js

--- commands/info/google.js
+++ commands/info/google.js
@@ -12,28 +12,44 @@ module.exports = {
         };
         const keyword = message.content.substr(message.content.indexOf(' ') + 1);
         const response = await google.search(keyword, options);
-        console.log(response);
-        console.log(message.flags);
         message.channel.send({
-            embed: {
+            embeds: [{
                 title: response.results[0].title,
                 url: response.results[0].url,
                 description: response.results[0].description,
                 color: 'BLUE',
-            },
+            }],
         });
         const images = await google.image(keyword, options);
-        console.log(images);
-        console.log(images[0].url);
-        message.channel.send({
-            embed: {
-                title: images[0].origin.title,
-                url: images[0].origin.website,
-                image: {
-                    url: images[0].url,
-                },
-                color: 'BLUE',
-            },
-        });
+        const cleanPanel = { ...response.knowledge_panel };
+        delete cleanPanel.title;
+        delete cleanPanel.description;
+        delete cleanPanel.url;
+        delete cleanPanel.images;
+        delete cleanPanel.type;
+        for (const [key, value] of Object.entries(response.knowledge_panel)) {
+            if (value === 'N/A') delete response.knowledge_panel[key];
+        }
+        console.log(response.knowledge_panel);
+        if (response.knowledge_panel && Object.keys(response.knowledge_panel).length !== 0) {
+            const fields = [];
+            for (const [key, value] of Object.entries(cleanPanel)) {
+                const entry = {};
+                entry['name'] = key;
+                entry['value'] = value;
+                fields.push(entry);
+            }
+            console.log(fields);
+            message.channel.send({
+                embeds: [{
+                    title: `Knowledge Panel: ${response.knowledge_panel.title ?? 'None'}`,
+                    description: response.knowledge_panel.description,
+                    url: response.knowledge_panel.url,
+                    fields: fields,
+                    image: { url: response.knowledge_panel.images?.[0] ?? images?.[0]?.url ?? '' },
+                }],
+            });
+        }
+
     },
 };
