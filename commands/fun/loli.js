482745d328483f0b2b6796c5563fa837dfc2d2ff
ChinHongTan <52919589+ChinHongTan@users.noreply.github.com>
2022-04-02 23:45:54 +0800

    Update loli.js

--- commands/fun/loli.js
+++ commands/fun/loli.js
@@ -8,7 +8,7 @@ const refreshToken = process.env.PIXIV_REFRESH_TOKEN || require('../../config/co
 async function loli(command, language) {
     const pixiv = await Pixiv.default.refreshLogin(refreshToken);
     const word = 'Chino Kafuu';
-    let illusts = await pixiv.search.illusts({ word: word, r18: false, type:'illust', bookmarks: '500', search_target: 'partial_match_for_tags' });
+    let illusts = await pixiv.search.illusts({ word: word, r18: false, type:'illust', bookmarks: '1000', search_target: 'partial_match_for_tags' });
     if (pixiv.search.nextURL) illusts = await pixiv.util.multiCall({ next_url: pixiv.search.nextURL, illusts }, 3);
     const sorted_illusts = pixiv.util.sort(illusts);
     const randomIllust = sorted_illusts[Math.floor(Math.random() * sorted_illusts.length)];
