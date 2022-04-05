d874cb4f22b0be28d73401ab390f13ca31e52d2a
ChinHongTan <chinhongtan1@gmail.com>
2022-04-05 16:51:59 +0800

    Update loli.js

--- commands/fun/loli.js
+++ commands/fun/loli.js
@@ -7,9 +7,7 @@ const refreshToken = process.env.PIXIV_REFRESH_TOKEN || require('../../config/co
 const fs = require('fs');
 
 // search pixiv for loli pictures
-async function loli(command, args) {
-    const num = args[0] ?? 1;
-    if (args[0] > 10) return commandReply.reply(command, 'Number cannot exceed 10!', 'RED');
+async function loli(command) {
     const pixiv = await Pixiv.default.refreshLogin(refreshToken);
     const word = 'Chino Kafuu';
     let illusts;
@@ -23,38 +21,45 @@ async function loli(command, args) {
         if (pixiv.search.nextURL) illusts = await pixiv.util.multiCall({ next_url: pixiv.search.nextURL, illusts }, 3);
     }
 
-    const embeds = [];
     const multipleIllusts = [];
-    for (let i = 0; i < num; i++) {
-        // choose an illust randomly and send it
-        const randomIllust = illusts[Math.floor(Math.random() * illusts.length)];
-        const targetURL = randomIllust.meta_pages.length === 0 ? `https://pixiv.cat/${randomIllust.id}.png` : `https://pixiv.cat/${randomIllust.id}-1.png`;
-        if (num >= 2 && num <= 4) {
-            const multipleIllust = new MessageEmbed()
-                .setURL('https://www.pixiv.net')
-                .setImage(targetURL)
-            multipleIllusts.push(multipleIllust);
-        }
-        const embed = new MessageEmbed()
-            .setTitle(randomIllust.title)
-            .setURL(`https://www.pixiv.net/en/artworks/${randomIllust.id}`)
-            .setColor('RANDOM')
-            // remove html tags
-            .setDescription(randomIllust
-                ?.caption
-                .replace(/\n/ig, '')
-                .replace(/<style[^>]*>[\s\S]*?<\/style[^>]*>/ig, '')
-                .replace(/<head[^>]*>[\s\S]*?<\/head[^>]*>/ig, '')
-                .replace(/<script[^>]*>[\s\S]*?<\/script[^>]*>/ig, '')
-                .replace(/<\/\s*(?:p|div)>/ig, '\n')
-                .replace(/<br[^>]*\/?>/ig, '\n')
-                .replace(/<[^>]*>/ig, '')
-                .replace('&nbsp;', ' ')
-                .replace(/[^\S\r\n][^\S\r\n]+/ig, ' '))
-            .setImage(multipleIllusts.length === 0 ? targetURL : null);
-        embeds.push(embed);
+    const targetURL = [];
+    const embeds = [];
+    // choose an illust randomly and send it
+    const randomIllust = illusts[Math.floor(Math.random() * illusts.length)];
+    if (randomIllust.meta_pages.length === 0 || randomIllust.meta_pages.length > 5) {
+        targetURL.push(`https://pixiv.cat/${randomIllust.id}.png`);
     }
-    if (num >= 2 && num <= 4) {
+    else {
+        for (let i = 1; i <= randomIllust.meta_pages.length; i++) {
+            targetURL.push(`https://pixiv.cat/${randomIllust.id}-${i}.png`);
+        }
+    }
+    targetURL.forEach((URL) => {
+        const multipleIllust = new MessageEmbed()
+            .setURL('https://www.pixiv.net')
+            .setImage(URL);
+        multipleIllusts.push(multipleIllust);
+    });
+
+    const embed = new MessageEmbed()
+        .setTitle(randomIllust.title)
+        .setURL(`https://www.pixiv.net/en/artworks/${randomIllust.id}`)
+        .setColor('RANDOM')
+        // remove html tags
+        .setDescription(randomIllust
+            ?.caption
+            .replace(/\n/ig, '')
+            .replace(/<style[^>]*>[\s\S]*?<\/style[^>]*>/ig, '')
+            .replace(/<head[^>]*>[\s\S]*?<\/head[^>]*>/ig, '')
+            .replace(/<script[^>]*>[\s\S]*?<\/script[^>]*>/ig, '')
+            .replace(/<\/\s*(?:p|div)>/ig, '\n')
+            .replace(/<br[^>]*\/?>/ig, '\n')
+            .replace(/<[^>]*>/ig, '')
+            .replace('&nbsp;', ' ')
+            .replace(/[^\S\r\n][^\S\r\n]+/ig, ' '))
+        .setImage(multipleIllusts.length === 0 ? targetURL[0] : null);
+    embeds.push(embed);
+    if (multipleIllusts.length !== 0) {
         return await commandReply.edit(command, { embeds: multipleIllusts.concat(embeds), components: [], content: '\u200b' });
     }
     await commandReply.edit(command, { embeds: embeds, components: [], content: '\u200b' });
@@ -63,19 +68,19 @@ module.exports = {
     name: 'loli',
     cooldown: 3,
     description: true,
-    async execute(message, _args, language) {
+    async execute(message) {
         if (!refreshToken) return message.reply('This command can\'t be used without pixiv refreshToken!');
         const repliedMessage = await message.reply('Please wait...');
-        await loli(repliedMessage, language);
+        await loli(repliedMessage);
     },
     slashCommand: {
         data: new SlashCommandBuilder()
             .addIntegerOption((option) =>
                 option.setName('number').setDescription('Number of illusts to be send at once')),
-        async execute(interaction, language) {
+        async execute(interaction) {
             if (!refreshToken) return interaction.reply('This command can\'t be used without pixiv refreshToken!');
             await interaction.deferReply();
-            await loli(interaction, [interaction.options.getInteger('number') ?? 1], language);
+            await loli(interaction);
         },
     },
 };
