23880a25713b67935b4328a690d4d0b608047bc4
ChinHongTan <chinhongtan1@gmail.com>
2022-04-07 12:44:40 +0800

    fix bug

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -23,7 +23,7 @@ module.exports = {
         if (stars) {
             const star = /^⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
             const foundStar = stars.embeds[0];
-            const image = message.attachments.size > 0 ? await extension(reaction, message.attachments.array()[0].url) : '';
+            const image = message.attachments.size > 0 ? await extension(reaction, message.attachments.first().url) : '';
             const embed = new MessageEmbed()
                 .setColor(foundStar.color)
                 .setDescription(foundStar.description)
@@ -35,7 +35,7 @@ module.exports = {
             await starMsg.edit({ embeds: [embed] });
         }
         if (!stars) {
-            const image = message.attachments.size > 0 ? await extension(reaction, message.attachments.array()[0].url) : '';
+            const image = message.attachments.size > 0 ? await extension(reaction, message.attachments.first().url) : '';
             if (image === '' && message.cleanContent.length < 1) return message.channel.send(`${user}, you cannot star an empty message.`);
             const embed = new MessageEmbed()
                 .setColor(15844367)
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -22,7 +22,7 @@ module.exports = {
         if (stars) {
             const star = /^⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
             const foundStar = stars.embeds[0];
-            const image = message.attachments.size > 0 ? await extension(reaction, message.attachments.array()[0].url) : '';
+            const image = message.attachments.size > 0 ? await extension(reaction, message.attachments.first().url) : '';
             const embed = new MessageEmbed()
                 .setColor(foundStar.color)
                 .setDescription(foundStar.description)
