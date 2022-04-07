931d1b6b7037106f76e40441ced3a7067942b032
ChinHongTan <chinhongtan1@gmail.com>
2022-04-07 12:24:17 +0800

    change depreciated code

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -21,15 +21,15 @@ module.exports = {
         const fetchedMessages = await starChannel.messages.fetch({ limit: 100 });
         const stars = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(message.id));
         if (stars) {
-            const star = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
+            const star = /^⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
             const foundStar = stars.embeds[0];
             const image = message.attachments.size > 0 ? await extension(reaction, message.attachments.array()[0].url) : '';
             const embed = new MessageEmbed()
                 .setColor(foundStar.color)
                 .setDescription(foundStar.description)
-                .setAuthor(message.author.tag, message.author.displayAvatarURL)
+                .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL })
                 .setTimestamp()
-                .setFooter(`⭐ ${parseInt(star[1]) + 1} | ${message.id}`)
+                .setFooter({ text: `⭐ ${parseInt(star[1]) + 1} | ${message.id}` })
                 .setImage(image);
             const starMsg = await starChannel.messages.fetch(stars.id);
             await starMsg.edit({ embeds: [embed] });
@@ -40,9 +40,9 @@ module.exports = {
             const embed = new MessageEmbed()
                 .setColor(15844367)
                 .setDescription(message.cleanContent)
-                .setAuthor(message.author.tag, message.author.displayAvatarURL)
+                .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL })
                 .setTimestamp(new Date())
-                .setFooter(`⭐ 1 | ${message.id}`)
+                .setFooter({ text: `⭐ 1 | ${message.id}` })
                 .setImage(image);
             await starChannel.send({ embeds: [embed] });
         }
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -20,15 +20,15 @@ module.exports = {
         const fetchedMessages = await starChannel.messages.fetch({ limit: 100 });
         const stars = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(reaction.message.id));
         if (stars) {
-            const star = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
+            const star = /^⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
             const foundStar = stars.embeds[0];
             const image = message.attachments.size > 0 ? await extension(reaction, message.attachments.array()[0].url) : '';
             const embed = new MessageEmbed()
                 .setColor(foundStar.color)
                 .setDescription(foundStar.description)
-                .setAuthor(message.author.tag, message.author.displayAvatarURL)
+                .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL })
                 .setTimestamp()
-                .setFooter(`⭐ ${parseInt(star[1]) - 1} | ${message.id}`)
+                .setFooter({ text: `⭐ ${parseInt(star[1]) - 1} | ${message.id}` })
                 .setImage(image);
             const starMsg = await starChannel.messages.fetch(stars.id);
             await starMsg.edit({ embeds: [embed] });
