dfc666fc9d9b3c222bcd26286037eaddd2a4a9f1
ChinHongTan <chinhongtan1@gmail.com>
2022-04-19 22:47:47 +0800

    Create get-backup-file.js

---
+++ commands/backup/get-backup-file.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { reply } = require('../../functions/commandReply.js');
+const fs = require('fs');
+
+async function getBackupFile(command, args, language) {
+    const backupID = args[0];
+    if (!backupID) return reply(command, language.invalidBackupID, 'RED');
+    let rawData;
+    try {
+        rawData = fs.readFileSync(`./my-backups/${backupID}.json`);
+    } catch (err) {
+        return reply(command, language.noBackupFound.replace('${backupID}', backupID), 'RED');
+    }
+    return reply(command, { content: `Backup file ID: \`${backupID}\``, files: rawData });
+}
+
+module.exports = {
+    name: 'load',
+    cooldown: 10,
+    guildOnly: true,
+    permissions: 'ADMINISTRATOR',
+    description: {
+        'en_US': 'Load a server backup based on backup ID.',
+        'zh_CN': '根据ID加载备份文件',
+        'zh_TW': '根據ID加載備份文件',
+    },
+    async execute(message, args, language) {
+        await getBackupFile(message, args, language);
+    },
+    slashCommand: {
+        data: new SlashCommandBuilder()
+            .addIntegerOption((option) => option.setName('id').setDescription('backup id')),
+        async execute(interaction, language) {
+            await getBackupFile(interaction, [interaction.option.getInteger('id')], language);
+        },
+    },
+};
